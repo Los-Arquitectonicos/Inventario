67118c53cf4846b3007d4509b17c2b02758e1aae
Pedro Pablo S. Trujillo <123520249+PedroSTrujillo@users.noreply.github.com>
2025-12-01 09:01:17 -0500

    Remove duplicate import of admin in URL configuration

--- ProvesiWMS/wms/urls.py
+++ ProvesiWMS/wms/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from django.contrib import admin
+
 from django.urls import include, path
 from rest_framework_simplejwt.views import TokenRefreshView
 
